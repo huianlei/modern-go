011baf1c5eb8b7380fc0a15e8ec937cb00ff2984
anlei.hui <anlei.hui@funplus.com>
2026-01-28 10:41:34 +0800

    feat: upgrade go version to 1.22

--- gls/g/g_test.go
+++ gls/g/g_test.go
@@ -1,8 +1,10 @@
 package g
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -39,5 +41,45 @@ func TestG2(t *testing.T) {
 		if gp2 == gp1 {
 			t.Fatalf("every living G must be different. [gp1:%p] [gp2:%p]", gp1, gp2)
 		}
+	})
+}
+
+func TestUnsafe(t *testing.T) {
+	type Header struct {
+		Id    int64
+		Magic uint32
+		Len   uint32
+	}
+
+	hdr := Header{Id: 123, Magic: 9999, Len: 8}
+	p := unsafe.Pointer(&hdr)
+	buf := (*[8]byte)(unsafe.Pointer(&hdr))[:]
+	fmt.Printf("sizeOf hdr.Magic: %v\n", unsafe.Sizeof(hdr.Magic))
+	fmt.Printf("buf: %v\n", buf)
+
+	idPtr := unsafe.Pointer(uintptr(p))
+	fmt.Printf("point addr: %v\n", uintptr(p))
+	fmt.Printf("idPtr: %v\n", idPtr)
+	fmt.Printf("Id: %v\n", *(*int64)(idPtr))
+
+	magicPtr := unsafe.Pointer(uintptr(p) + unsafe.Sizeof(hdr.Id))
+	fmt.Printf("magicPtr: %v\n", magicPtr)
+	fmt.Printf("Magic: %v\n", *(*uint32)(magicPtr))
+
+	lenPtr := unsafe.Pointer(uintptr(p) + unsafe.Sizeof(hdr.Id) + unsafe.Sizeof(hdr.Magic))
+	fmt.Printf("lenPtr: %v\n", lenPtr)
+	fmt.Printf("Len: %v\n", *(*uint32)(lenPtr))
+}
+
+func TestGoroutineG(t *testing.T) {
+	gp1 := G()
+	assert.True(t, gp1 != nil)
+	fmt.Printf("gp1: %v\n", gp1)
+
+	t.Run("G in another goroutine", func(t *testing.T) {
+		gp2 := G()
+		fmt.Printf("gp2: %v\n", gp2)
+		assert.True(t, gp2 != nil)
+		assert.True(t, gp1 != gp2)
 	})
 }
--- go.mod
+++ go.mod
@@ -1,10 +1,19 @@
 module github.com/funbytes/modern-go
 
-go 1.13
+go 1.22
 
 require (
 	github.com/smartystreets/goconvey v1.6.4
 	github.com/stretchr/testify v1.8.0
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
 	golang.org/x/sys v0.0.0-20210608053332-aa57babbf139
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20181017120253-0766667cb4d1 // indirect
+	github.com/jtolds/gls v4.20.0+incompatible // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/smartystreets/assertions v0.0.0-20180927180507-b2de0cb4f26d // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
